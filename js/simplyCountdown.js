297baa36eebefff54be07f16c9ff90a8273ef5ac
LAPTOP-N8EBOCNB\nyomansony <nyoman.sony@gmail.com>
2023-10-08 01:15:38 +0700

    - update schedule

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -111,10 +111,10 @@
     exports.simplyCountdown = (elt, args) => {
         const eltProto = Object.getPrototypeOf(elt);
         let parameters = extend({
-            year: 2015,
-            month: 6,
-            day: 28,
-            hours: 9,
+            year: 2023,
+            month: 12,
+            day: 24,
+            hours: 7,
             minutes: 0,
             seconds: 0,
             words: {
